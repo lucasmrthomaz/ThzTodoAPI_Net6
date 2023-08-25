eebd12e43d546d91f02876c3250ca3cccc941426
Lucas Thomaz <lucasmrthomaz@outlook.com>
2023-08-25 01:05:09 -0300

    Initial Commit

---
+++ Models/TodoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ThzTodoAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        
+        public TodoContext(IConfiguration configuration, DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Uses SQLite3 for simple data storage.
+            options.UseSqlite("DataSource=ThzTodoListAPI.db");
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
